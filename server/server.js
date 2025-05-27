e50c7762f6dcb0ebffb877dcfa704541dd0c5c2e
kavinraj-e <kavinrajeswaramoorthi@gmail.com>
2025-05-27 15:00:09 +0530

    url update

--- server/server.js
+++ server/server.js
@@ -19,4 +19,6 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/tasks", taskRoutes);
 
-app.listen(5000, () => console.log("Server started on port 5000"));
+const PORT = process.env.PORT || 5000;  // fallback to 5000 if PORT not set
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
